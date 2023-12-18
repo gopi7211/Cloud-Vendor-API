b23d3451f55522b81207ac5cbefcee7fbe4727b3
GOPALA KRISHNA J <79375218+gopi7211@users.noreply.github.com>
2023-12-18 17:21:13 +0530

    Cloud-Vendor-API

---
+++ cloud-vendor-api/src/main/java/com/spring/cloudvendorapi/Controller/CloudVendorApiService.java
@@ -0,0 +1,38 @@
+package com.spring.cloudvendorapi.Controller;
+
+import com.spring.cloudvendorapi.model.CloudVendor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cloudvendor")
+public class CloudVendorApiService
+{
+    CloudVendor cloudVendor;
+
+    @GetMapping("{vendorId}")
+    public CloudVendor getCloudVendorDetails(String vendorId)
+    {
+        return cloudVendor;
+    }
+
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor)
+    {
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor Created Successfully";
+    }
+
+    @PutMapping
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor)
+    {
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor Updated Successfully";
+    }
+
+    @DeleteMapping("{vendorId}")
+    public String deleteCloudVendorDetails(String vendorId)
+    {
+        this.cloudVendor = null;
+        return "Cloud Vendor Deleted Successfully";
+    }
+}
